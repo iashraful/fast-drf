b2d7e87c22f12617bf5734ae7a35ba57e18390b6
Ashraful Islam <ashrafulrobin3@gmail.com>
2018-12-26 16:45:23 +0600

    * Fixed queryset bug * Added docstring on common classes

--- fast_drf/core/api_generator.py
+++ fast_drf/core/api_generator.py
@@ -1,5 +1,5 @@
-from rest_framework.generics import ListCreateAPIView
 from rest_framework import serializers
+from rest_framework.generics import ListCreateAPIView
 
 
 class APIGenerator(object):
@@ -14,14 +14,33 @@ class APIGenerator(object):
         self.serializer_class = kwargs.get('serializer_class', self.get_runtime_serializer(**kwargs))
         self.viewset_class = kwargs.get('viewset_class')
         self.permission_classes = kwargs.get('permission_classes', [])
-        self.queryset = kwargs.get('queryset', [])
+        self.queryset = kwargs.get('queryset', self.get_queryset(**kwargs))
+
+    def get_queryset(self, *args, **kwargs):
+        """
+        Queryset maker for model
+        :param args:
+        :param kwargs:
+        :return: a queryset
+        """
+        return self.model.objects.all()
 
     def get_runtime_viewset(self, **kwargs):
+        """
+        Viewset finder and return
+        :param kwargs: all the extra params are accepted and pass to child
+        :return: a viewset class
+        """
         if self.viewset_class is None:
             return self.make_runtime_viewset(**kwargs)
         return self.viewset_class
 
     def make_runtime_viewset(self, **func_kwargs):
+        """
+        Viewset Maker while it's not available on model. It's a generic viewset maker for any model
+        :param func_kwargs: all the extra params are accepted and pass to child
+        :return: return a viewset
+        """
         class RunTimeViewset(ListCreateAPIView):
             if self.permission_classes:
                 permission_classes = self.permission_classes
@@ -34,11 +53,21 @@ class APIGenerator(object):
         return RunTimeViewset
 
     def get_runtime_serializer(self, **kwargs):
+        """
+        Serializer finder and return
+        :param kwargs: all the extra params are accepted and pass to child
+        :return: return a serializer class
+        """
         if self.serializer_class is None and self.model:
             return self.make_runtime_serializer(**kwargs)
         return self.serializer_class
 
     def make_runtime_serializer(self, **func_kwargs):
+        """
+        A generic serializer maker at server first run
+        :param func_kwargs: all the extra params are accepted and pass to child
+        :return: return serializer class
+        """
         class RuntimeModelSerializer(serializers.ModelSerializer):
             class Meta:
                 model = self.model
