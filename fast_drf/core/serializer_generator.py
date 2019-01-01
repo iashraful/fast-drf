7ba7e5cdf1474dc15a10db33060495d561d13314
Ashraful Islam <ashrafulrobin3@gmail.com>
2019-01-02 05:43:05 +0600

    minor fixes

--- fast_drf/core/serializer_generator.py
+++ fast_drf/core/serializer_generator.py
@@ -5,13 +5,13 @@ class SerializerGenerator(object):
     def __init__(self, *args, **kwargs):
         self.model = kwargs.get('model')
 
-
     def make_runtime_serializer(self, **func_kwargs):
         """
-        A generic serializer maker at server first run
+        A generic serializer maker once at a time
         :param func_kwargs: all the extra params are accepted and pass to child
         :return: return serializer class
         """
+
         class RuntimeModelSerializer(serializers.ModelSerializer):
             class Meta:
                 model = self.model
