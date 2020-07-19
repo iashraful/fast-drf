ea4530fb7d94783a2712f5e8a199b0cb3fee10a4
Mohammad Ashraful Islam <ashrafulrobin3@gmail.com>
2020-07-19 09:24:27 +0600

    Updated to v1.0.10 and added changelog

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='fast-drf',
-    version='1.0.9',
+    version='1.0.10',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',
